2c5bf32fbcc1145978f35cb16622e8ba1b2536fc
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-07-16 17:19:16 +0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,12 +15,3 @@
 #   end
 # end
 # config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins '*'
-    resource '*',
-      headers: :any,
-      expose: ['Authorization'],
-      methods: [:get, :post, :patch, :put, :delete, :options, :head]
-  end
-end
