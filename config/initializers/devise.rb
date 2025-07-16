c82c36a124f2ecd5a012b34039fc92cdd2867017
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-07-16 17:42:31 +0300

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -307,6 +307,21 @@ Devise.setup do |config|
 
   # ==> Configuration for :registerable
 
+
+
+
+
+config.jwt do |jwt|
+  jwt.secret = ENV['DEVISE_JWT_SECRET_KEY'] || 'your_secret_here'
+  jwt.dispatch_requests = [
+    ['POST', %r{^/api/v1/login$}]
+  ]
+  jwt.revocation_requests = [
+    ['DELETE', %r{^/api/v1/logout$}]
+  ]
+  jwt.expiration_time = 1.day.to_i
+end
+
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
