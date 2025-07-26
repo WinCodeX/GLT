afea6c7154d13dedf8b1cba967c937fb7b7a7d02
WinCodeX <glenwinterg970@gmail.com>
2025-07-26 11:08:15 +0300

    new

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+["admin", "agent", "rider", "client", "warehouse"].each do |role|
+  Role.find_or_create_by(name: role)
+end
