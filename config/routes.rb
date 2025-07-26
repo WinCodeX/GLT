3201203751a17214a58c31ecf6608c6d18b982f8
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-07-26 12:05:38 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users,
-           path: 'api/v1',
-           controllers: {
-             sessions: 'api/v1/sessions'
-           }
+  path: 'api/v1',
+  path_names: {
+    sign_in: 'login',
+    sign_out: 'logout'
+  },
+  controllers: {
+    sessions: 'api/v1/sessions'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
