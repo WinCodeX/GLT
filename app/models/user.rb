63dc113e119dd1ca8291f6d152f25306c40eec05
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-07-29 07:57:55 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,21 @@
 class User < ApplicationRecord
+  # Include default devise modules + JWT
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
+
+  # ActiveStorage for avatar
+  has_one_attached :avatar
+
+  # Rolify for roles
   rolify
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-devise :database_authenticatable, :registerable,
-       :recoverable, :rememberable, :validatable,
-       :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 
-
-
-has_one_attached :avatar
-
-  rolify
+  # Default role after creation
   after_create :assign_default_role
 
+  private
+
   def assign_default_role
-    self.add_role(:client) if self.roles.blank?
+    add_role(:client) if roles.blank?
   end
 end
