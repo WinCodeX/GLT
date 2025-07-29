b5de1167521100e761ac06310d0b6dab1a546c1a
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-07-29 17:04:48 +0300

    Update business.rb

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,3 +1,5 @@
 class Business < ApplicationRecord
-  belongs_to :user
+  belongs_to :user   # the owner
+  has_many :business_memberships
+  has_many :members, through: :business_memberships, source: :user
 end
