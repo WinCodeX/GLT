83b1b1ccff4d68d60cfe7889d70fec9a775b0125
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-07-30 22:39:02 +0300

    Update user_serializer.rb

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,5 @@
-require 'net/http'
-require 'uri'
-
 class UserSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
-  include ActionController::MimeResponds
+  include UrlHostHelper
 
   attributes :id, :email, :roles, :avatar_url
 
@@ -13,26 +9,6 @@ class UserSerializer < ActiveModel::Serializer
 
   def avatar_url
     return unless object.avatar.attached?
-
-    available_host = first_available_host
-    return unless available_host
-
-    rails_blob_url(object.avatar, host: available_host)
-  end
-
-  private
-
-  def first_available_host
-    Rails.configuration.x.avatar_hosts.each do |host|
-      begin
-        # Ping only the root of the domain to test availability
-        uri = URI.parse(host)
-        response = Net::HTTP.get_response(uri)
-        return host if response.is_a?(Net::HTTPSuccess) || response.is_a?(Net::HTTPRedirection)
-      rescue StandardError
-        next
-      end
-    end
-    nil
+    fallback_host_url_for(object.avatar)
   end
 end
