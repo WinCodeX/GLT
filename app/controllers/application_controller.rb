6e9e9aaf58de0544bcd221b13f24e9fba08ca96a
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-08-02 14:09:14 +0300

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 # app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
-  # Only include this if you added modules like Devise::Controllers::Helpers
-  include ActionController::RequestForgeryProtection
-
-  protect_from_forgery with: :null_session
 end
