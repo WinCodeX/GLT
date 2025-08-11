fcd8dc234d39fba875f4bc5080a052672ed1de17
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-08-11 16:48:47 +0300

    Update location_serializer.rb

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,29 +1,9 @@
-# app/serializers/location_serializer.rb
 class LocationSerializer
-  include ActiveModel::Serializers::JSON
-
-  def initialize(location)
-    @location = location
-  end
-
-  def as_json(options = {})
-    {
-      id: @location.id.to_s,
-      name: @location.name,
-      initials: safe_initials(@location),
-      created_at: @location.created_at&.iso8601,
-      updated_at: @location.updated_at&.iso8601
-    }
-  end
-
-  def self.serialize_collection(locations)
-    locations.map { |location| new(location).as_json }
-  end
-
-  private
-
-  def safe_initials(location)
-    # Try the initials attribute first, then generate from name
+  include FastJsonapi::ObjectSerializer
+  
+  attributes :name, :created_at, :updated_at
+  
+  attribute :initials do |location|
     if location.respond_to?(:initials) && location.initials.present?
       location.initials
     elsif location.respond_to?(:safe_initials)
@@ -32,8 +12,10 @@ class LocationSerializer
       generate_initials(location.name)
     end
   end
-
-  def generate_initials(name)
+  
+  private
+  
+  def self.generate_initials(name)
     return '' if name.blank?
     
     words = name.split(' ')
