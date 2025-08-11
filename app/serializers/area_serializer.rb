9bbab6f4226e6190363df8b74e1abffc74ce42e4
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-08-11 16:49:46 +0300

    Update area_serializer.rb

--- app/serializers/area_serializer.rb
+++ app/serializers/area_serializer.rb
@@ -1,49 +1,20 @@
-# app/serializers/area_serializer.rb
 class AreaSerializer
-  include ActiveModel::Serializers::JSON
-
-  def initialize(area)
-    @area = area
-  end
-
-  def as_json(options = {})
-    {
-      id: @area.id.to_s,
-      name: @area.name,
-      initials: safe_initials(@area),
-      location_id: @area.location_id.to_s,
-      location: location_data,
-      created_at: @area.created_at&.iso8601,
-      updated_at: @area.updated_at&.iso8601
-    }
-  end
-
-  def self.serialize_collection(areas)
-    areas.includes(:location).map { |area| new(area).as_json }
-  end
-
-  private
-
-  def safe_initials(area)
-    # Try the initials attribute first, then generate from name
+  include FastJsonapi::ObjectSerializer
+  
+  attributes :name, :location_id, :created_at, :updated_at
+  belongs_to :location, serializer: LocationSerializer
+  
+  attribute :initials do |area|
     if area.respond_to?(:initials) && area.initials.present?
       area.initials
     else
       generate_initials(area.name)
     end
   end
-
-  def location_data
-    return nil unless @area.location
-    
-    {
-      id: @area.location.id.to_s,
-      name: @area.location.name,
-      initials: safe_initials(@area.location)
-    }
-  end
-
-  def generate_initials(name)
+  
+  private
+  
+  def self.generate_initials(name)
     return '' if name.blank?
     
     words = name.split(' ')
