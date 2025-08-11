d572d8dece384ce1f5d471b558e9af9a0d2c1aef
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-08-11 13:25:11 +0300

    Update locations_controller.rb

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -5,23 +5,47 @@ module Api
       before_action :authenticate_user!
 
       def index
-        locations = Location.includes(:areas)
-        render json: locations.as_json(include: :areas)
+        locations = Location.all.order(:name)
+        
+        render json: {
+          success: true,
+          locations: LocationSerializer.serialize_collection(locations)
+        }
+      end
+
+      def show
+        location = Location.find(params[:id])
+        render json: {
+          success: true,
+          location: LocationSerializer.new(location).as_json
+        }
+      rescue ActiveRecord::RecordNotFound
+        render json: {
+          success: false,
+          message: 'Location not found'
+        }, status: :not_found
       end
 
       def create
         location = Location.new(location_params)
         if location.save
-          render json: location, status: :created
+          render json: {
+            success: true,
+            location: LocationSerializer.new(location).as_json,
+            message: 'Location created successfully'
+          }, status: :created
         else
-          render json: { errors: location.errors.full_messages }, status: :unprocessable_entity
+          render json: {
+            success: false,
+            errors: location.errors.full_messages
+          }, status: :unprocessable_entity
         end
       end
 
       private
 
       def location_params
-        params.require(:location).permit(:name)
+        params.require(:location).permit(:name, :initials)
       end
     end
   end
