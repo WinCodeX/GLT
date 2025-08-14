7e226b037cd1004f50b15475b3b7d809f08255a7
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-08-14 11:11:34 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -45,6 +45,7 @@ Rails.application.routes.draw do
 resources :users, only: [] do
       collection do
         get :scan_history
+       get :scanning_stats
       end
     end
       # ==========================================
