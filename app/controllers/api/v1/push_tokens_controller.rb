213399e4a80f5daa8669cfb71113546ca6c1f0d1
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-09-14 15:27:16 +0300

    Update push_tokens_controller.rb

--- app/controllers/api/v1/push_tokens_controller.rb
+++ app/controllers/api/v1/push_tokens_controller.rb
@@ -1,5 +1,5 @@
 # app/controllers/api/v1/push_tokens_controller.rb
-class Api::V1::PushTokensController < Api::V1::BaseController
+class Api::V1::PushTokensController < ApplicationController
   before_action :authenticate_user!
   
   # POST /api/v1/push_tokens
