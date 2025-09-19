548d2d5f417171b4538cb7913af4ca93db8c55ef
WinCodeX <123963612+WinCodeX@users.noreply.github.com>
2025-09-19 21:13:20 +0300

    Update businesses_controller.rb

--- app/controllers/api/v1/businesses_controller.rb
+++ app/controllers/api/v1/businesses_controller.rb
@@ -2,8 +2,8 @@ module Api
   module V1
     class BusinessesController < ApplicationController
       before_action :authenticate_user!
-      before_action :set_business, only: [:show, :update, :destroy, :staff]
-      before_action :authorize_business_access, only: [:show, :staff]
+      before_action :set_business, only: [:show, :update, :destroy, :staff, :activities]
+      before_action :authorize_business_access, only: [:show, :staff, :activities]
       before_action :authorize_business_owner, only: [:update, :destroy]
 
       def create
